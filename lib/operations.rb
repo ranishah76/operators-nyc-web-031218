b25bb97e2f5cfcf6e47a72377619deeae12606ee
Rani Shah <rani76@gmail.com>
2018-02-26 18:17:43 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-if speed < 40 && speed > 60
-	 true
-else
-	 false
+if speed > 60 
+	true 
+elsif speed < 40 
+	true
+else 
+	false 
+end 
 end
 
 
 
 def not_safe?(speed)
-40 <= speed <= 60 ? false : true
+speed < 40 || speed > 60 > true : false 
 end
